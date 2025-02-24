f002038808313bbef389413c9f9de74076f4124f
stanipetrosyan <stani.petrosyan@gmail.com>
2025-02-24 21:59:41 +0100

    feat: get player pref to setup settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -10,8 +8,24 @@ public class SettingsMenu : MonoBehaviour{
     [SerializeField] private AudioMixer audioMixer;
     private Resolution[] resolutions;
     [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Toggle fullscreenToggle;
+    [SerializeField] private TMP_Dropdown qualityDropdown;
 
-
+    
+    private void Start() {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        var options = resolutions.Select(item => $"{item.width}x{item.height}").ToList();
+        resolutionDropdown.AddOptions(options);
+        
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1);
+        fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", 0) == 1;
+        qualityDropdown.value = PlayerPrefs.GetInt("Quality", 0);
+        
+        int resolution = PlayerPrefs.GetInt("Resolution", 0);
+        Screen.SetResolution(resolutions[resolution].width, resolutions[resolution].height, Screen.fullScreen);
+    }
     public void SetVolume(float volume) {
         audioMixer.SetFloat("Volume", volume);
         
@@ -29,17 +43,7 @@ public class SettingsMenu : MonoBehaviour{
         
         PlayerPrefs.SetInt("Fullscreen", fullscreen ? 1 : 0);
     }
-
-    private void Start() {
-        resolutions = Screen.resolutions;
-        
-        resolutionDropdown.ClearOptions();
-
-        var options = resolutions.Select(item => $"{item.width}x{item.height}").ToList();
-
-        resolutionDropdown.AddOptions(options);
-    }
-
+    
     public void SetResolution(int resolutionIndex) {
         Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, Screen.fullScreen);
         
