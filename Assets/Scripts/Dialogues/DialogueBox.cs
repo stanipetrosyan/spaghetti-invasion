5e06516eaccd53787e80cba08395d5533c62b20d
stanipetrosyan <stani.petrosyan@gmail.com>
2025-03-12 20:47:28 +0100

    fix: dialogue manager and dialoguw system

--- Assets/Scripts/Dialogues/DialogueBox.cs
+++ Assets/Scripts/Dialogues/DialogueBox.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace Dialogues {
-    public class DialogueBox: MonoBehaviour {
-        public Image characterIcon;
-        public TextMeshProUGUI characterName;
-        public TextMeshProUGUI dialogueArea;
-        
-        private Queue<DialogueLine> dialogueLines = new();
+    public class DialogueBox : MonoBehaviour {
+        [SerializeField] private Image characterIcon;
+        [SerializeField] private TextMeshProUGUI characterName;
+        [SerializeField] private TextMeshProUGUI dialogueArea;
+        [SerializeField] private Button nextDialogueButton;
+
+        private readonly Queue<DialogueLine> dialogueLines = new();
 
         public bool isDialogueActive = false;
 
-        public float typingSpeed = 0.2f;
-        
+        private const float TypingSpeed = 0.5f;
+
+        private void Start() {
+            nextDialogueButton.onClick.AddListener(DisplayNextDialogueLine);
+        }
+
         public void StartDialogue(Dialogue dialogue) {
             if (isDialogueActive) return;
 
             isDialogueActive = true;
-            
+
             dialogueLines.Clear();
 
             foreach (var line in dialogue.lines) {
@@ -30,12 +35,12 @@ namespace Dialogues {
             DisplayNextDialogueLine();
         }
 
-        private void DisplayNextDialogueLine() {
-            if (dialogueLines.Count ==  0) {
+        void DisplayNextDialogueLine() {
+            if (dialogueLines.Count == 0) {
                 EndDialogue();
                 return;
-            }  
-            
+            }
+
             var currentLine = dialogueLines.Dequeue();
 
             characterIcon.sprite = currentLine.character.icon;
@@ -50,7 +55,7 @@ namespace Dialogues {
             dialogueArea.text = "";
             foreach (var letter in currentLine.line) {
                 dialogueArea.text += letter;
-                yield return new WaitForSeconds(typingSpeed);
+                yield return new WaitForSeconds(TypingSpeed);
             }
         }
 
