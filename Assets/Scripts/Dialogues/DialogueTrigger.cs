781fcbb9c6bac6166d21eee710251cf55f3a2b6c
stanipetrosyan <stani.petrosyan@gmail.com>
2025-03-20 20:56:33 +0100

    feat: improved dialogue box

--- Assets/Scripts/Dialogues/DialogueTrigger.cs
+++ Assets/Scripts/Dialogues/DialogueTrigger.cs
@@ -1,5 +1,4 @@
 using Managers;
-using Port;
 using UnityEngine;
 
 namespace Dialogues {
--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,13 +1,21 @@
 using Port;
+using UI;
 using UnityEngine;
 
 namespace Managers {
     public class InputManager: MonoBehaviour, IManager {
+        [SerializeField] private Popup settings;
         private bool canMove;
         public ManagerStatus Status { get; set; }
         public void Startup() {
             canMove = true;
             Status = ManagerStatus.Started;
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                // open game menu
+            }
         }
 
         public bool CanMove() {
