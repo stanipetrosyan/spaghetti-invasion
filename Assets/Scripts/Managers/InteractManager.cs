81e4a5fc09bf9d75babe815797f1c05828a44134
stanipetrosyan <stani.petrosyan@gmail.com>
2025-03-22 12:54:42 +0100

    fix: interact manager

--- Assets/Scripts/Managers/InteractManager.cs
+++ Assets/Scripts/Managers/InteractManager.cs
@@ -1,25 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using Managers;
 using Port;
 using TMPro;
 using UnityEngine;
-using UnityEngine.WSA;
 
-public class InteractManager : MonoBehaviour, IManager{
-    [SerializeField] private TextMeshProUGUI text;
+namespace Managers {
+    public class InteractManager : MonoBehaviour, IManager{
+        [SerializeField] private TextMeshProUGUI text;
 
-    public void Activate() {
-        text.gameObject.SetActive(true);
-    }
+        public void Activate() {
+            text.gameObject.SetActive(true);
+        }
 
-    public void Deactivate() {
-        text.gameObject.SetActive(false);
-    }
+        public void Deactivate() {
+            text.gameObject.SetActive(false);
+        }
 
-    public ManagerStatus Status { get; set; }
-    public void Startup() {
-        Deactivate();
-        Status = ManagerStatus.Started;
+        public ManagerStatus Status { get; set; }
+        public void Startup() {
+            Deactivate();
+            Status = ManagerStatus.Started;
+        }
     }
 }
