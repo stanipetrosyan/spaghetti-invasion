881c5b96374ed62b9ba314f5a1ce712b578a4da5
stanipetrosyan <stani.petrosyan@gmail.com>
2025-03-27 23:00:28 +0100

    feat: added prison guard concept

---
+++ Assets/Scripts/Enemies/PrisonGuard.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemies {
+    public class PrisonGuard: MonoBehaviour {
+        public float radius;
+        
+        private void Update() {
+            Debug.DrawCircle(transform.position, radius, 32, Color.green);
+            
+            var hits = new List<RaycastHit2D>();
+            Physics2D.CircleCast(transform.position, radius, Vector2.zero, new ContactFilter2D().NoFilter(), hits, radius);
+
+            foreach (var hit in hits) {
+                if (hit.collider is not null) {
+                    var player = hit.collider.gameObject.GetComponent<Player>();
+                    if (player) {
+                        player.Transform();
+                    }
+                    UnityEngine.Debug.DrawLine(transform.position, hit.point, Color.red);
+                }    
+            }
+        
+        }
+    }
+}
