595d7150559c12ce93f324cbca47458f4d3427b5
stanipetrosyan <stani.petrosyan@gmail.com>
2025-04-04 21:45:08 +0200

    feat: added anxiety sound and improve anxiety effect

--- Assets/Scripts/AnxietyCounter.cs
+++ Assets/Scripts/AnxietyCounter.cs
@@ -1,13 +1,12 @@
 using Managers;
-using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
-using UnityEngine.Serialization;
 
 public class AnxietyCounter : MonoBehaviour {
     public float anxiety;
     public float smoothTime = 1.0f;
     private float time = 0f;
+    public float test = 0f;
 
     [SerializeField] private Light2D light2D;
     [SerializeField] private Camera gameCamera;
@@ -15,30 +14,48 @@ public class AnxietyCounter : MonoBehaviour {
     private float minimum = 26f;
     private float maximum;
 
+
     private void Start() {
         maximum = gameCamera.orthographicSize;
     }
 
     private void Update() {
-        if (anxiety <= 1) return;
-        
+        if (anxiety <= 1) {
+            GameManagers.Audio.StopHeartBeat();
+            return;
+        }
+
+        GameManagers.Audio.PlayHeartBeat();
         HeartBeat();
         RedScreen();
     }
 
     private void RedScreen() {
-        var green = (255 - anxiety*10) / 255;
-        var blue = (255 - anxiety*10) / 255;
-        
-        light2D.color = new Color(1, green,  blue, 1);
+        var green = (255 - anxiety * 10) / 255;
+        var blue = (255 - anxiety * 10) / 255;
+
+        light2D.color = new Color(1, green, blue, 1);
     }
 
     private void HeartBeat() {
         gameCamera.orthographicSize = Mathf.Lerp(minimum, maximum, time);
 
-        time += anxiety * Time.deltaTime;
+        if (Mathf.Approximately(maximum, 30)) {
+            time += test * Time.deltaTime;
+        }
+        else {
+            time += test * 3 * Time.deltaTime;
+        }
+
 
         if (time > smoothTime) {
+            if (maximum > minimum) {
+                minimum -= 0.2f;
+            }
+            else {
+                maximum -= 0.2f;
+            }
+
             (maximum, minimum) = (minimum, maximum);
             time = 0.0f;
         }
@@ -52,11 +69,11 @@ public class AnxietyCounter : MonoBehaviour {
 
         anxiety += amount * Time.deltaTime;
     }
-    
+
 
     public void DecreaseAnxiety(float amount) {
         if (anxiety <= 0) return;
-        
+
         anxiety -= amount * Time.deltaTime;
     }
 }
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 namespace Managers {
     public class AudioManager: MonoBehaviour, IManager {
         [SerializeField] private AudioSource prisonAudio;
-        
+        [SerializeField] private AudioSource heartBeatAudio;
         public ManagerStatus Status { get; set; }
         public void Startup() {
             Status = ManagerStatus.Started;
         }
 
         private void Start() {
-            prisonAudio.Play();
-            Debug.Log("Playing prison audio");
+            //prisonAudio.Play();
+        }
+
+        public void PlayHeartBeat() {
+            if(heartBeatAudio.isPlaying) return;
+            
+            heartBeatAudio.Play();
+        }
+        
+        
+        public void StopHeartBeat() {
+            heartBeatAudio.Stop();
         }
     }
 }
