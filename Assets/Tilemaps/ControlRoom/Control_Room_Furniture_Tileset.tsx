efc008aa493fa572b1b04b23933730ef9795a8a6
stanipetrosyan <stani.petrosyan@gmail.com>
2025-04-11 18:07:49 +0200

    feat: implemented control room

---
+++ Assets/Tilemaps/ControlRoom/Control_Room_Furniture_Tileset.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Control_Room_Furniture" tilewidth="96" tileheight="96" tilecount="6" columns="3">
+ <image source="Tiles Sala comando interni.png" trans="ff00ff" width="369" height="192"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="21" y="55" width="30" height="20.7083"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="34.875" y="46.125" width="39.125" height="26.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="30" y="9" width="66" height="69.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="9" width="48" height="69.0909"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Assets/Tilemaps/ControlRoom/Control_Room_Tileset.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Control_Room_tileset" tilewidth="96" tileheight="96" tilecount="16" columns="4">
+ <image source="Tiles Sala comando 1.png" trans="ff00ff" width="384" height="384"/>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="96" height="96"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="96" height="96"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="96" height="96"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="12" y="18" width="84" height="54"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18" width="81" height="54"/>
+  </objectgroup>
+ </tile>
+</tileset>
