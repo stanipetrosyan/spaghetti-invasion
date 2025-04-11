987122e9140b428585693dd68836d4edc220a610
stanipetrosyan <stani.petrosyan@gmail.com>
2025-04-11 18:35:06 +0200

    feat: added elevator map

---
+++ Assets/Tilemaps/Elevator/Elevator_Tileset.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Elevator_Tileset" tilewidth="96" tileheight="96" tilecount="4" columns="2">
+ <image source="Ascensore.png" trans="ff00ff" width="192" height="192"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11.6667" y="0" width="69.6667" height="95.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="96" height="96"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="96" height="96"/>
+  </objectgroup>
+ </tile>
+</tileset>
